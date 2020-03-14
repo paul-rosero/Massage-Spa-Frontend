a95958dd4913de30abc2bed456ec23cb533349a8
Paul Rosero <paul_rosero@yahoo.com>
2020-03-14 19:58:38 -0400

    add code to show all therapist available.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,10 +1,23 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  const apptsList = document.querySelector('#appointments-list')
+})
+
+fetch('http://localhost:3000/massage_therapsists', {method: 'get'})
+.then(resp =>resp.json())
+.then(therapistsDataJson => {
+  therapistsDataJson.forEach(therapist =>{
+    const newTherapist = new MassageTherapist(therapist)
+
+  })
+})  
+
 document.addEventListener('DOMContentLoaded', () => {
     const endpoint = 'http://localhost:3000/api/v1/appointments';
     fetch(endpoint)
       .then(res => res.json())
       .then(json => json.forEach(appointment => {
         const newAppt = new Appointment(appointment);
-        document.querySelector('#appointments-list').innerHTML += newAppt.renderDetails();
+        apptsList.innerHTML += newAppt.renderDetails();
       })
     );
   });

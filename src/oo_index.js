48f803e15279c6782e26678ab2bc8b76d78adb23
Paul Rosero <paul_rosero@yahoo.com>
2020-03-14 17:19:18 -0400

    refactor eventlistener so its more dry and less code.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,19 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const appointments = 'http://localhost:3000/api/v1/appointments';
-    fetch(appointments)
+    const endpoint = 'http://localhost:3000/api/v1/appointments';
+    fetch(endpoint)
       .then(res => res.json())
       .then(json => json.forEach(appointment => {
-        const markup = `
-        <li>
-            <h4>Appointment ${appointment.id}</h4>
-            <p>Massage Therapist: ${appointment.massage_therapist.name}</p>
-            <p>Client: ${appointment.client.name}</p>
-            <p>Modality:${appointment.modality}</p>
-            <p>Appointment Time: ${appointment.appointment_time}</p>
-            <p>Special Requests: ${appointment.special_request}</p><br>
-        </li>`;
-
-        document.querySelector('#appointment-list').innerHTML += markup;
+        const newAppt = new Appointment(appointment);
+        document.querySelector('#appointment-list').innerHTML += newAppt.renderDetails();
       })
     );
   });

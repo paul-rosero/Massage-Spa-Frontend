723a6e5396b6f47ae4418d71aed2688c94f4f5f2
Paul Rosero <paul_rosero@yahoo.com>
2020-03-14 17:18:35 -0400

    refactor code to have more dry code.

--- src/appointment.js
+++ src/appointment.js
@@ -1,4 +1,13 @@
 class Appointment {
+    constructor(apptDataObj) {
+        this.id = apptDataObj.id
+        this.massage_therapist = apptDataObj.massage_therapist
+        this.client = apptDataObj.client
+        this.modality = apptDataObj.modality
+        this.appointment_time = apptDataObj.appointment_time
+        this.special_request = apptDataObj.special_request
+        Appointment.allAppointments.push(this)
+    }
     static findAppointment(id) {
         return this.allAppointments.find((appointment) => appointment.id === id)
     }
@@ -11,28 +20,21 @@ class Appointment {
         apptToUpdate.appointment_time = updatedApptData.appointment_time
         apptToUpdate.special_request = updatedApptData.special_request
         return apptToUpdate
-    }
-
-    constructor(apptDataObj) {
-        this.id = apptDataObj.id
-        this.massage_therapist = apptDataObj.massage_therapist
-        this.client = apptDataObj.client
-        this.modality = apptDataObj.modality
-        this.appointment_time = apptDataObj.appointment_time
-        this.special_request = apptDataObj.special_request
-        Appointment.allAppointments.push(this)
-    }
+    }p
     
     renderSpan(){
         return `<span-data id="${this.id}">${this.name}</span>`
     }
 
     renderDetails(){
-        return `<h4>"${this.massage_therapist}"</h4>
-                <h4>"${this.client}"</h4>        
-                <h4>"${this.modality}"</h4>
-                <h4>"${this.appointment_time}"</h4>
-                <h4>"${this.special_request}"</h4>`
+        return `<h4>Appointment: ${this.id}
+                    <button data-id=${this.id}>Edit Appointment</button>
+                </h4><br>
+                <p>Massage Therapist: ${this.massage_therapist.name}</p><br>
+                <p>Client: ${this.client.name}</p><br>
+                <p>Modality: ${this.modality}</p><br>
+                <p>Appointment Time: ${this.appointment_time}</p><br>
+                <p>Special Requests: ${this.special_request}</p><br>`
     }
 }
 

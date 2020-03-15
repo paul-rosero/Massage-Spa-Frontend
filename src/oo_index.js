6ec6013137baf62f784338521c8fa3a7d687f01c
Paul Rosero <paul_rosero@yahoo.com>
2020-03-15 15:28:40 -0400

    add code to be able to see an appointment when clicked on.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,24 +1,31 @@
 document.addEventListener('DOMContentLoaded', () =>{
+  const therapistsList = document.querySelector('#all-therapists-list')
+  const apptsList = document.querySelector('#appointments-list')
+  const apptInfoList = document.querySelector('#appointment-info-list')
+
+  
   fetch('http://localhost:3000/api/v1/massage_therapists', {method: 'GET'})
   .then(resp =>resp.json())
   .then(therapistsDataJson => {
     therapistsDataJson.forEach(therapist =>{
-      const therapistsList = document.querySelector('#all-therapists-list')
       const newTherapist = new MassageTherapist(therapist)
       therapistsList.innerHTML += newTherapist.renderSpan()
     })
-  })  
-})
-  document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:3000/api/v1/appointments', {method: 'GET'})
-    .then(res => res.json())
-    .then(json => json.forEach(appointment => {
-      const apptsList = document.querySelector('#appointments-list')
-      const newAppt = new Appointment(appointment);
-      apptsList.innerHTML += newAppt.renderDetails();
-    }))
   })
-
+  
+  fetch('http://localhost:3000/api/v1/appointments', {method: 'GET'})
+  .then(res => res.json())
+  .then(json => json.forEach(appointment => {
+    const newAppt = new Appointment(appointment);
+    apptsList.innerHTML += newAppt.renderSpan();
+  }))
+  
+  apptsList.addEventListener('click', (e) => {
+    const clickedAppt = parseInt(e.target.dataset.id)
+    const foundAppt = Appointment.findAppointment(clickedAppt)
+    apptInfoList.innerHTML = foundAppt.renderDetails()
+  })
+})
 
 
 

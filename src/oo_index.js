97274b33f0d60607e25b8a16660a1b92c17e6985
Paul Rosero <paul_rosero@yahoo.com>
2020-03-14 21:45:20 -0400

    rearrange code so it loads better and faster.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', () =>{
-  const apptsList = document.querySelector('#appointments-list')
+  fetch('http://localhost:3000/api/v1/massage_therapists', {method: 'GET'})
+  .then(resp =>resp.json())
+  .then(therapistsDataJson => {
+    therapistsDataJson.forEach(therapist =>{
+      const therapistsList = document.querySelector('#all-therapists-list')
+      const newTherapist = new MassageTherapist(therapist)
+      therapistsList.innerHTML += newTherapist.renderSpan()
+    })
+  })  
 })
-
-fetch('http://localhost:3000/massage_therapsists', {method: 'get'})
-.then(resp =>resp.json())
-.then(therapistsDataJson => {
-  therapistsDataJson.forEach(therapist =>{
-    const newTherapist = new MassageTherapist(therapist)
-
+  document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:3000/api/v1/appointments', {method: 'GET'})
+    .then(res => res.json())
+    .then(json => json.forEach(appointment => {
+      const apptsList = document.querySelector('#appointments-list')
+      const newAppt = new Appointment(appointment);
+      apptsList.innerHTML += newAppt.renderDetails();
+    }))
   })
-})  
 
-document.addEventListener('DOMContentLoaded', () => {
-    const endpoint = 'http://localhost:3000/api/v1/appointments';
-    fetch(endpoint)
-      .then(res => res.json())
-      .then(json => json.forEach(appointment => {
-        const newAppt = new Appointment(appointment);
-        apptsList.innerHTML += newAppt.renderDetails();
-      })
-    );
-  });
+
+
+
+  

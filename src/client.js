954592f97e82b435031d6628b5a6edb34c689f47
Paul Rosero <paul_rosero@yahoo.com>
2020-06-13 17:38:50 -0400

    comment out code that is not necessary for now, more for future code

--- src/client.js
+++ src/client.js
@@ -8,28 +8,28 @@ class Client {
         Client.allClients.push(this)
     }
 
-    static findClient(id) {
-        return this.allClients.find((client) => client.id === id)
-    }
+//     static findClient(id) {
+//         return this.allClients.find((client) => client.id === id)
+//     }
 
-    static updateClient(updatedClientData) {
-        const clientToUpdate = this.findClient(updatedClientData.id)
-        clientToUpdate.massage_therapist = updatedClientData.massage_therapist
-        clientToUpdate.client = updatedClientData.client
-        clientToUpdate.modality = updatedClientData.modality
-        clientToUpdate.appointment_time = updatedClientData.appointment_time
-        clientToUpdate.special_request = updatedClientData.special_request
-        return clientToUpdate
-    }
+//     static updateClient(updatedClientData) {
+//         const clientToUpdate = this.findClient(updatedClientData.id)
+//         clientToUpdate.massage_therapist = updatedClientData.massage_therapist
+//         clientToUpdate.client = updatedClientData.client
+//         clientToUpdate.modality = updatedClientData.modality
+//         clientToUpdate.appointment_time = updatedClientData.appointment_time
+//         clientToUpdate.special_request = updatedClientData.special_request
+//         return clientToUpdate
+//     }
     
-    renderDetails() {
-        return `<br><h4>Client Info.</h4>
-                <p>Client: ${this.id}</p>
-                <p>Name: ${this.name}</p>
-                <p>Medical History: ${this.medical_history}</p>
-                <p>Address: ${this.address}</p>
-                <p>Email: ${this.email}</p>`
-    }
+//     renderDetails() {
+//         return `<br><h4>Client Info.</h4>
+//                 <p>Client: ${this.id}</p>
+//                 <p>Name: ${this.name}</p>
+//                 <p>Medical History: ${this.medical_history}</p>
+//                 <p>Address: ${this.address}</p>
+//                 <p>Email: ${this.email}</p>`
+//     }
 }
 
 Client.allClients = []

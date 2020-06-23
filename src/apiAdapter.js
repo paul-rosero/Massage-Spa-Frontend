57c43410932bd73e63546c33928ca963fae49f0e
Paul Rosero <paul_rosero@yahoo.com>
2020-06-22 21:17:34 -0400

    add fetchSelect function to fetch all the data

--- src/apiAdapter.js
+++ src/apiAdapter.js
@@ -8,5 +8,22 @@ class apiAdapter {
             .then(promise => promise.json())
     }
 
+    fetchSelect(url, input) {
+        let defaultOption = document.createElement('OPTION');
+        input.add(defaultOption);
+        return fetch(this.baseUrl + url)  
+        .then(function(response) {   
+            response.json().then(function(appt) {  
+                let option;
+                for (let i = 0; i < appt.length; i++) {
+                    option = document.createElement('option');
+                    option.dataset.id = appt[i].id;
+                    option.text = appt[i].name;
+                    option.value = appt[i].name;
+                    input.add(option);
+                }    
+            });  
+        })  
+    }
     
 }

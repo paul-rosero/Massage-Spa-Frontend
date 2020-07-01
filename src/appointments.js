016a11ed29922cb2f996305a4f8d26d6957eb894
Paul Rosero <paul_rosero@yahoo.com>
2020-07-01 16:21:18 -0400

    add functions plus a constructor with empty array

--- src/appointments.js
+++ src/appointments.js
@@ -1,5 +1,15 @@
 class Appointments {
   constructor() {
     console.log("appointments are loaded")
+    this.appointments = []
   }
+
+  static findAppointment(id) {
+    return Appointments.find((appointment) => appointment.id === id)
+  }
+
+  renderSpan(){
+      return `<span data-id="${this.id}">Appointment ${this.id}</span><br><br>`
+  }
+
 }

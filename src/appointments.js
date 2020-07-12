7f538f362885e73f5dfc8fa2bfc8e2b99f776412
Paul Rosero <paul_rosero@yahoo.com>
2020-07-12 16:03:44 -0400

    add ApiAdapter variable, comment out code for use

--- src/appointments.js
+++ src/appointments.js
@@ -2,7 +2,15 @@ class Appointments {
   constructor() {
     console.log("appointments are loaded")
     this.appointments = []
+    this.adapter = new ApiAdapter()
+    this.getAllAppointments()
+    // this.client = new Client()
+    // this.appointment = new Appointment()
+    // this.therapist = new MassageTherapist()
+    // this.bindVariables()
+    // this.addEventListeners()
   }
+
 
   static findAppointment(id) {
     return Appointments.find((appointment) => appointment.id === id)

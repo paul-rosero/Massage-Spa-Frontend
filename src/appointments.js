561bb70a1d9e5eb6cfc47c58be0e1688f5ff7654
Paul Rosero <paul_rosero@yahoo.com>
2020-07-15 23:29:34 -0400

    move variables to bindVariables and move fetch to allContentLoaded

--- src/appointments.js
+++ src/appointments.js
@@ -3,16 +3,18 @@ class Appointments {
     console.log("appointments are loaded")
     this.appointments = []
     this.adapter = new ApiAdapter()
+    this.bindVariables()
     this.allContentLoaded()
     // this.client = new Client()
     this.updateAppointment()
-    this.bindVariables()
     this.addEventListeners()
   }
 
   bindVariables(){
     this.apptsContainer = document.querySelector('#appointments-container');
     this.apptInfoList = document.querySelector('#appointment-info-list');
+    this.clientNameInput = document.querySelector('#client-name-input');
+    this.therapistNameInput = document.querySelector('#therapist-name-input');
   }
 
   addEventListeners(){
@@ -24,19 +26,18 @@ class Appointments {
   }
 
   allContentLoaded() {
-    this.adapter.fetchApi("appointments", { method: 'GET' }, this.appointments, Appointment)
-    .then(() => { this.renderLi() });
+    this.adapter.fetchApi("appointments", { method: 'GET' }, this.appointments, Appointment).then(() => { this.renderLi() });
     
-    this.adapter.fetchApi("massage_therapists", { method: 'GET' }, MassageTherapist.allTherapists, MassageTherapist)
-    .then(() => { MassageTherapist.renderDetails() });
-  }
-
-  updateAppointment(){
-    this.clientNameInput = document.querySelector('#client-name-input');
-    this.therapistNameInput = document.querySelector('#therapist-name-input');
+    this.adapter.fetchApi("massage_therapists", { method: 'GET' }, MassageTherapist.allTherapists, MassageTherapist).then(() => { MassageTherapist.renderDetails() });
     
     this.adapter.fetchSelect("massage_therapists", this.therapistNameInput);
     this.adapter.fetchSelect("clients", this.clientNameInput);
+  }
+
+  updateAppointment(){
+    
+    
+   
   }
 
   static findAppointment(id) {

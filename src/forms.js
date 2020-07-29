dafa11b43239a8e9798f89fb8a285b3724649cc0
Paul Rosero <paul_rosero@yahoo.com>
2020-07-29 18:19:09 -0400

    create class Form to display  all types of forms

---
+++ src/forms.js
@@ -0,0 +1,35 @@
+class Forms {
+    constructor(){
+        this.renderAppForm()
+    }
+
+    renderAppForm(){
+        const appointmentForm = document.getElementById("new-appointment-container")
+        appointmentForm.innerHTML = `
+            <form id="appointment-form" action="index.html" method="post">
+                <br/><h3>Appointment Form</h3>
+                <label for="name">Client Name:</label>
+                <select id="client-name-input" type="text" name="name" form="appointment-form" value=""><select><br/>
+    
+                <label for="name">Massage Therapist Name:</label>
+                <select id="therapist-name-input" type="text" name="name" form="appointment-form" value=""></select><br/>
+    
+                <label for="modality">Modality:</label>
+                <input id="modality-input" type="text" name="modality" value=""/><br/>
+    
+                <label for="time">Appointment Time:</label>
+                <input id="appointment-time-input" type="text" name="time" value=""/><br/>
+    
+                <label for="request">Special Requests:</label>
+                <input id="special-request-input" type="text" name="request" value=""/><br/>
+    
+                <button id="create-button" name="create-button">Create Appointment</button>
+                <input type="button" id="edit-button" name="edit-button" value="Edit Appointment"/>
+            </form>
+        `
+        
+        console.log(appointmentForm)
+
+        
+    }
+}

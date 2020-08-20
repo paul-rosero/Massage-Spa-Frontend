3223463131573be3fdae1044e0a5dc835b7c504a
Paul Rosero <paul_rosero@yahoo.com>
2020-08-20 19:26:45 -0400

    refactor and delete code, add renderNewClientForm

--- src/client.js
+++ src/client.js
@@ -6,29 +6,27 @@ class Client {
         this.address = clientDataObj.address
         this.email = clientDataObj.email
         this.medical_history = clientDataObj.medical_history
+        this.renderNewClientForm()
     }
 
-//     static findClient(id) {
-//         return this.allClients.find((client) => client.id === id)
-//     }
+    findClient(id) {
+        return Client.allClients.find((client) => client.id === id)
+    }
 
-//     static updateClient(updatedClientData) {
-//         const clientToUpdate = this.findClient(updatedClientData.id)
-//         clientToUpdate.massage_therapist = updatedClientData.massage_therapist
-//         clientToUpdate.client = updatedClientData.client
-//         clientToUpdate.modality = updatedClientData.modality
-//         clientToUpdate.appointment_time = updatedClientData.appointment_time
-//         clientToUpdate.special_request = updatedClientData.special_request
-//         return clientToUpdate
-//     }
+    renderNewClientForm(){
+        Forms.renderClientForm()
+        console.log("object")
+    }
     
     renderDetails() {
-        return `<br><h4>Client Info.</h4>
-                <p>Client: ${this.id}</p>
-                <p>Name: ${this.name}</p>
-                <p>Medical History: ${this.medical_history}</p>
-                <p>Address: ${this.address}</p>
-                <p>Email: ${this.email}</p>`
+        return `
+            <br><h4>Client Info.</h4>
+            <p>Client: ${this.id}</p>
+            <p>Name: ${this.name}</p>
+            <p>Medical History: ${this.medical_history}</p>
+            <p>Address: ${this.address}</p>
+            <p>Email: ${this.email}</p>
+        `
     }
 }
 
